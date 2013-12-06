f36d923dd4b64600204ad9f09c9c8311aa2b29c2
Albert Tugushev <metalikus@gmail.com>
2013-12-06 18:15:15 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     install_requires=['Django>=1.3'],
     include_package_data=True,
     license='MIT License',
-    description='A very simple django settings.',
+    description='A very simple settings configurable in Django Admin Panel.',
     long_description=README,
     url='https://github.com/alikus/django-simple-settings',
     author='Albert Tugushev',
